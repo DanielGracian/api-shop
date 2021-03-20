489a1d1e380d8673a5d0af9f71c256b8d5a31433
Daniel <daniel.gracian@oktana.com>
2021-03-20 12:24:41 -0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
       api_key:
-        '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'
+        'SG.W3hO-hnyQkGPpZ0NJpcQXQ.oKN9W5NrLhZEchT9Zghx7TEZuwon7jGYW9CWg-KupQ0'
     }
   })
 );
